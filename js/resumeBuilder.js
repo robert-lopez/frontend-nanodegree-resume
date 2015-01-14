c2401c0c36e570781a353964dd85cead0a95f0a9
myself447 <myself447@gmail.com>
2015-01-14 14:28:16 -0400

    formats and appends skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -48,6 +48,21 @@ var projects = {"projects":[{
 ]
 }
 
+if(bio.skills.length > 0){
+
+	$("#header").append(HTMLskillsStart);
+	
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0])
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[1])
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[2])
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[3])
+	$("#skills").append(formattedSkill);
+
+}
+
 /*$("#header").prepend(HTMLskills.replace("%data%", bio.skills));
 
 $("#header").prepend(HTMLemail.replace("%data%", bio.contact.email));
